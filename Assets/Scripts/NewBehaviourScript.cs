67d88d669308c7f8a987cae91d7fe80db35ecfe0
James <James.J.Jiang.1@gmail.com>
2022-08-03 14:02:53 -0700

    Added Encoder
    Added resetting Encoder value and placing Encoder value into a variable called Evalue

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Encoder = new Encoder(0,1,2);
+    int EValue = 0;
+    public void resetEncoder(){
+        Encoder.reset();
+    }
+
+    
+    void Start()
+    {
+        Encoder.reset()
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Evalue = int Encoder.get();
+    }
+}
