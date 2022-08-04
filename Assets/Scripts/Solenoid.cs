37867cf4d5699d35a318cbb7f89baa1d034cd2d0
JustinTsai800 <98719229+JustinTsai800@users.noreply.github.com>
2022-08-03 18:39:10 -0700

    Added a Solenoid Class

---
+++ Assets/Scripts/Solenoid.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solenoid
+{
+    public bool set(bool on)
+    {
+        return on;
+    }
+
+    public double setPulseDuration(double durationSeconds)
+    {
+        return durationSeconds;
+    }
+}
